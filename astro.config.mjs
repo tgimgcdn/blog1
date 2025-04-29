e57595b660b02270d1d6f10a8a12e24f9aea4458
tgimgcdn <imgcdn.rr.nu@gassky.eu.org>
2025-04-29 18:53:44 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,39 @@ export default defineConfig({
             starlightImageZoom()
             ],
 			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			sidebar: [
+				{
+					label: '首页',
+					link: '/',
+					translations: { 'zh': '首页' },
+					attrs: { 'data-toc-hidden': 'true' }
+				},
+				{
+					label: '技术',
+					autogenerate: { directory: 'technology' },
+					translations: { 'zh': '技术' }
+				},
+				{
+					label: '娱乐',
+					autogenerate: { directory: 'entertainment' },
+					translations: { 'zh': '娱乐' }
+				},
+				{
+					label: '随笔',
+					autogenerate: { directory: 'essay' },
+					translations: { 'zh': '随笔' }
+				},
+				{
+					label: '项目',
+					items: [
+						{
+							label: '项目名称',
+							link: 'https://example.com'
+						}
+					],
+					translations: { 'zh': '项目' }
+				}
+			],
 			sidebar: [
 				{
 					label: 'Guides',
