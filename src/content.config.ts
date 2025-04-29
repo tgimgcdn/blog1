c40594b14f1171892b134cad27c7e78ff816f35e
tgimgcdn <imgcdn.rr.nu@gassky.eu.org>
2025-04-29 20:02:10 +0800

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,14 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+	docs: defineCollection({ 
+		loader: docsLoader(), 
+		schema: docsSchema().extend({
+			author: z.string().optional(),
+			pubDate: z.string().transform((str) => new Date(str)).optional(),
+			updatedDate: z.string().transform((str) => new Date(str)).optional(),
+		})
+	}),
 };
