176293e7301bf55b3cf53507ad32694cb9a9fd2a
tgimgcdn <imgcdn.rr.nu@gassky.eu.org>
2025-04-29 23:02:08 +0800

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,16 +3,11 @@ import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 
 const baseSchema = docsSchema();
-const extendedSchema = {
-	...baseSchema,
-	author: z.string().optional(),
-	pubDate: z.date().optional(),
-	updatedDate: z.date().optional(),
-};
 
 export const collections = {
 	docs: defineCollection({ 
 		loader: docsLoader(), 
-		schema: z.object(extendedSchema)
+		schema: baseSchema,
+		index: true
 	}),
 };
