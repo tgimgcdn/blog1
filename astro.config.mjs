ae54a3df569b96cfac0acd170e7b76af0f6b7df5
tgimgcdn <imgcdn.rr.nu@gassky.eu.org>
2025-04-29 19:00:06 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,59 +6,50 @@ import starlightImageZoom from 'starlight-image-zoom'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			plugins: [
-            starlightThemeFlexoki(),
-            starlightImageZoom()
-            ],
-			title: 'My Docs',
-			sidebar: [
-				{
-					label: '首页',
-					link: '/',
-					translations: { 'zh': '首页' },
-					attrs: { 'data-toc-hidden': 'true' }
-				},
-				{
-					label: '技术',
-					autogenerate: { directory: 'technology' },
-					translations: { 'zh': '技术' }
-				},
-				{
-					label: '娱乐',
-					autogenerate: { directory: 'entertainment' },
-					translations: { 'zh': '娱乐' }
-				},
-				{
-					label: '随笔',
-					autogenerate: { directory: 'essay' },
-					translations: { 'zh': '随笔' }
-				},
-				{
-					label: '项目',
-					items: [
-						{
-							label: '项目名称',
-							link: 'https://example.com'
-						}
-					],
-					translations: { 'zh': '项目' }
-				}
-			],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      plugins: [
+        starlightThemeFlexoki(),
+        starlightImageZoom()
+      ],
+      title: 'My Docs',
+      // 添加自定义组件配置
+      components: {
+        Footer: './src/components/Footer.astro'
+      },
+      sidebar: [
+        {
+          label: '首页',
+          link: '/',
+          translations: { 'zh': '首页' },
+          attrs: { 'data-toc-hidden': 'true' }
+        },
+        {
+          label: '技术',
+          autogenerate: { directory: 'technology' },
+          translations: { 'zh': '技术' }
+        },
+        {
+          label: '娱乐',
+          autogenerate: { directory: 'entertainment' },
+          translations: { 'zh': '娱乐' }
+        },
+        {
+          label: '随笔',
+          autogenerate: { directory: 'essay' },
+          translations: { 'zh': '随笔' }
+        },
+        {
+          label: '项目',
+          items: [
+            {
+              label: '项目名称',
+              link: 'https://example.com'
+            }
+          ],
+          translations: { 'zh': '项目' }
+        }
+      ],
+    }),
+  ],
 });
